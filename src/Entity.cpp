81cb23115d29fd8c8411128fa2a4bdd14b1e8ae5
Zubair Khan <Zubair@Zubairs-MacBook-Pro.local>
2018-07-27 20:44:08 -0500

    starting image in global

---
+++ src/Entity.cpp
@@ -0,0 +1,9 @@
+//
+//  Entity.cpp
+//  GameJamMaybe
+//
+//  Created by Zubair Khan on 7/27/18.
+//  Copyright © 2018 Zubair Khan. All rights reserved.
+//
+
+#include "Entity.hpp"
---
+++ src/Entity.hpp
@@ -0,0 +1,14 @@
+//
+//  Entity.hpp
+//  GameJamMaybe
+//
+//  Created by Zubair Khan on 7/27/18.
+//  Copyright © 2018 Zubair Khan. All rights reserved.
+//
+
+#ifndef Entity_hpp
+#define Entity_hpp
+
+#include <stdio.h>
+
+#endif /* Entity_hpp */
