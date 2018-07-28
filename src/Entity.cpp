973b4ca62a1074e4889486ea9427f24921c53fd4
Zubair Khan <Zubair@Zubairs-MacBook-Pro.local>
2018-07-27 20:54:37 -0500

    added entity

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -1,9 +1,99 @@
-//
-//  Entity.cpp
-//  GameJamMaybe
-//
-//  Created by Zubair Khan on 7/27/18.
-//  Copyright © 2018 Zubair Khan. All rights reserved.
-//
-
 #include "Entity.hpp"
+
+Entity::Entity(){
+    
+    x = 0;
+    y = 0;
+    xSpeed = 0;
+    ySpeed = 0;
+    health = 0;
+}
+
+Entity::~Entity(){
+    
+    image.free();
+}
+
+void Entity::updatePosition(int delta){
+    
+    x += (xSpeed * delta);
+    y += (ySpeed * delta);
+}
+
+int Entity::getX(){
+    
+    return x;
+}
+
+int Entity::getY(){
+    
+    return y;
+}
+
+int Entity::getWidth(){
+    
+    return image.getWidth();
+}
+
+int Entity::getHeight(){
+    
+    return image.getHeight();
+}
+
+int Entity::getHealth(){
+    
+    return health;
+}
+
+int Entity::getDX(){
+    
+    return xSpeed;
+}
+
+int Entity::getDY(){
+    
+    return ySpeed;
+}
+
+Texture& Entity::getImage(){
+    
+    return image;
+}
+
+void Entity::setX(int value){
+    
+    x = value;
+}
+
+void Entity::setY(int value){
+    
+    y = value;
+}
+
+void Entity::setDx(int value){
+    
+    xSpeed = value;
+}
+
+void Entity::setDy(int value){
+    
+    ySpeed = value;
+}
+
+void Entity::setHealth(int value){
+    
+    health = value;
+}
+
+void Entity::setImage(SDL_Renderer* renderer, std::string path){
+    
+    image.import(renderer, path);
+}
+
+bool Entity::getCollision(Entity &other){
+    
+    return (getX() >= other.getX() && getX() <= other.getX() + other.getWidth()
+            && getY() >= other.getY() && getY() <= other.getY() + other.getHeight()) ||
+    (other.getX() >= getX() && other.getX() <= getX() + getWidth()
+     && other.getY() >= getY() && other.getY() <= getY() + getHeight());
+}
--- src/Entity.hpp
+++ src/Entity.hpp
@@ -1,14 +1,43 @@
-//
-//  Entity.hpp
-//  GameJamMaybe
-//
-//  Created by Zubair Khan on 7/27/18.
-//  Copyright © 2018 Zubair Khan. All rights reserved.
-//
-
 #ifndef Entity_hpp
 #define Entity_hpp
 
-#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <iostream>
+
+class Entity {
+public:
+    Entity();
+    ~Entity();
+    
+    void updatePosition(int delta);
+    
+    int getX();
+    int getY();
+    int getDX();
+    int getDY();
+    int getWidth();
+    int getHeight();
+    int getHealth();
+    Texture& getImage();
+    
+    void setX(int value);
+    void setY(int value);
+    void setDx(int value);
+    void setDy(int value);
+    void setHealth(int value);
+    void setImage(SDL_Renderer* renderer, std::string path);
+    
+    bool getCollision(Entity &other);
+    
+private:
+    Texture image;
+    int x;
+    int y;
+    int xSpeed;
+    int ySpeed;
+    int health;
+};
 
 #endif /* Entity_hpp */
