57466ab8babcbb5c0ef80b079c17923e8224a57c
Zubair Khan <Zubair@Zubairs-MacBook-Pro.local>
2018-07-28 22:29:35 -0500

    increase difficulty with more time

--- src/main.cpp
+++ src/main.cpp
@@ -264,7 +264,8 @@ void update(int delta){
 
         level.update(delta);
 
-        if((SDL_GetTicks() - releaseMonster) >= 3000){
+        
+        if((SDL_GetTicks() - releaseMonster) >= level.getSpawnTime()){
             releaseMonster = SDL_GetTicks();
             level.createMonster();
         }
